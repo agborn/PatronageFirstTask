9edbcdb90ccf6bdb895575a51e84d3264d0541ad
Agata <aborninska@wi.zut.edu.pl>
2014-12-15 23:08:54 +0100

    pierwsze zadanie

---
+++ FirstTask/FirstTask/FirstTask.Shared/Place.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace FirstTask
+{
+    public class Place : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        private string address;
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+
+        private double latitude;
+        public double Latitude
+        {
+            get
+            {
+                return latitude;
+            }
+            set
+            {
+                latitude = value;
+            }
+        }
+
+        private double longitude;
+        public double Longitude
+        {
+            get
+            {
+                return longitude;
+            }
+            set
+            {
+                longitude = value;
+            }
+        }
+
+        private bool hasWifi;
+        public bool HasWifi
+        {
+            get
+            {
+                return hasWifi;
+            }
+            set
+            {
+                hasWifi = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "{" + Name + "}, {" + Address + "}";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+    }
+}
