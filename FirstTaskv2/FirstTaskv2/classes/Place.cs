15bf37e79ff0b8cc73e46aa74ef3f0c49b4a998f
Agata <aborninska@wi.zut.edu.pl>
2014-12-17 23:16:27 +0100

    rozwiazanie 1. zadania - v2

---
+++ FirstTaskv2/FirstTaskv2/classes/Place.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace FirstTaskv2.classes
+{
+    public class Place : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public async void Geolacation()
+        {
+            Geolocator geolocator = new Geolocator();
+            geolocator.DesiredAccuracyInMeters = 50;
+
+            Geoposition geoposition = await geolocator.GetGeopositionAsync(
+             maximumAge: TimeSpan.FromMinutes(5),
+             timeout: TimeSpan.FromSeconds(10)
+            );
+
+            Latitude = geoposition.Coordinate.Latitude;
+            Longitude = geoposition.Coordinate.Longitude;
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value != this.name)
+                {
+                    this.name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        private string address;
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                if (value != this.address)
+                {
+                    this.address = value;
+                    NotifyPropertyChanged("Address");
+                }
+            }
+        }
+
+        private double latitude;
+        public double Latitude
+        {
+            get
+            {
+                return latitude;
+            }
+            set
+            {
+                if (value != this.latitude)
+                {
+                    this.latitude = value;
+                    NotifyPropertyChanged("Latitude");
+                }
+            }
+        }
+
+        private double longitude;
+        public double Longitude
+        {
+            get
+            {
+                return longitude;
+            }
+            set
+            {
+                if (value != this.longitude)
+                {
+                    this.longitude = value;
+                    NotifyPropertyChanged("Longitude");
+                }
+            }
+        }
+
+        private bool hasWifi;
+        public bool HasWifi
+        {
+            get
+            {
+                return hasWifi;
+            }
+            set
+            {
+                if (value != this.hasWifi)
+                {
+                    this.hasWifi = value;
+                    NotifyPropertyChanged("HasWifi");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}", Name, Address);
+        }
+    }
+}
